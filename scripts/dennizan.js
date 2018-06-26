7ec56da114c7306168367ce37bc4332405b457e6
Denizhan Kilic <denizhankilic@gmail.com>
2018-06-26 14:59:51 +0200

    Searchbox now returns an array named arr with 20 results

--- scripts/dennizan.js
+++ scripts/dennizan.js
@@ -1,13 +1,18 @@
 const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
 const matchningURL = 'platsannonser/matchning?';
 const searchBox = document.querySelector( '#searchBox' );
+const searchSubmit = document.querySelector( '#searchSubmit' )
 const numberOfResults = 20; // document.querySelector( '#numberOfResults' );
 
 arr = [];
 
-let fritextSokning = function ( searchQuery ) {
+let fritextSokning = function ( event ) {
+  event.preventDefault();
 
-  let parameterString = 'sida=1&antalrader=' + numberOfResults + '&nyckelord=' + searchQuery;
+  if ( searchBox.value == "" ) {
+    return console.log( 'No search term entered' );
+  }
+  let parameterString = 'sida=1&antalrader=' + numberOfResults + '&nyckelord=' + searchBox.value;
 
   fetch( baseURL + matchningURL + parameterString )
     .then( response => {
@@ -18,3 +23,5 @@ let fritextSokning = function ( searchQuery ) {
     } );
 
 }
+
+searchSubmit.addEventListener( 'click', fritextSokning );
