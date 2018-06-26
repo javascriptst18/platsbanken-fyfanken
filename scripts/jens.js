98279faa5948bae2450c8c148ce58583469c3510
Jens Filipsson <jensfilipsson@gmail.com>
2018-06-26 15:13:28 +0200

    Expand and close function of job ads

--- scripts/jens.js
+++ scripts/jens.js
@@ -1,12 +1,23 @@
 const mainContainer = document.querySelector('main');
 
 mainContainer.addEventListener('click', function (e) {
-    if (e.target.id === 'expandJobAd') {
+    if (e.target.classList.contains('expand-job-ad')) {
         let articleNodes = document.querySelectorAll('article');
         for (let article of articleNodes) {
             article.classList.remove('expanded');
         }
         e.target.parentElement.classList.add('expanded');
+        let jobAdTarget = e.target.parentElement;
+        document.addEventListener('click', function (e) {
+            if (!e.target.closest('.expanded')) {
+                jobAdTarget.classList.remove('expanded');
+            }
+        });
+        document.addEventListener('keyup', event => {
+            if (event.key === 'Escape' || event.keyCode === 27) {
+                jobAdTarget.classList.remove('expanded');
+            }
+        });
         e.target.parentElement.scrollIntoView();
     }
 })
