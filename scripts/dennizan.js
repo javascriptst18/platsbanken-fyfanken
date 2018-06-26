9ac44520de482428401aa473696c1d468fb08671
Denizhan Kilic <denizhankilic@gmail.com>
2018-06-26 15:45:29 +0200

    Numb er of results can now be set

--- scripts/dennizan.js
+++ scripts/dennizan.js
@@ -2,7 +2,7 @@ const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
 const matchningURL = 'platsannonser/matchning?';
 const searchBox = document.querySelector( '#searchBox' );
 const searchSubmit = document.querySelector( '#searchSubmit' )
-const numberOfResults = 20; // document.querySelector( '#numberOfResults' );
+const numberOfResults = document.querySelector( '#numberOfResults' );
 
 // Array med sökningsresultat. Innehåller 10 annonser från Stockholms län när sidan laddas första gången. Uppdateras med search box sökningen när man clickar "Search"
 searchResultsArr = [];
@@ -23,7 +23,7 @@ let fritextSokning = function ( event ) {
   if ( searchBox.value == "" ) {
     return console.log( 'No search term entered' );
   }
-  let parameterString = 'sida=1&antalrader=' + numberOfResults + '&nyckelord=' + searchBox.value;
+  let parameterString = 'sida=1&antalrader=' + numberOfResults.value + '&nyckelord=' + searchBox.value;
 
   fetch( baseURL + matchningURL + parameterString )
     .then( response => {
